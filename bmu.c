cd029bf94647af79cb1b936e27d3da2997662f4a
Christine Seeliger <c.seeliger@garvan.org.au>
2014-12-17 13:59:45 +1100

    Wed 17/12/

---
+++ bmu.c
@@ -0,0 +1,25 @@
+/*
+ * bmu.c
+ *
+ *  Created on: 17 Dec 2014
+ *      Author: yeti
+ */
+
+
+#include "header.h"
+#include "bmu_agent_header.h"
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int create(){
+	return 0;
+}
+
+int die(){
+	return 1;
+}
+
+int move(){
+   return 0;
+}
